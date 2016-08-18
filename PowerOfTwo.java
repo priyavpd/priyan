d231bd830f915e3234d9d4cb151b7f50334471ed
priyavpd <vpriyacse2@gmail.com>
2016-08-18 04:24:59 -0700

    Create PowerOfTwo.java

---
+++ PowerOfTwo.java
@@ -0,0 +1,20 @@
+public class PowerOfTwo
+{  
+    public static void main(String args[])  
+    {  
+
+        Scanner in=new Scanner(System.in);
+        System.out.println("Enter the number : ");
+        int num = in.nextInt();
+
+        int other = 1;  
+        if(((~num) & 1) == 1)  
+        {  
+            System.out.println("The number is a power of two");  
+        }  
+        else  
+        {
+            System.out.println("The number is a  NOT A power of two");  
+        }
+    }  
+} 
