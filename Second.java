7067974d9927045e63d2024f51661ef4548317e8
priyavpd <vpriyacse2@gmail.com>
2016-08-18 05:12:55 -0700

    Create Second.java

---
+++ Second.java
@@ -0,0 +1,28 @@
+public class Second
+{
+public static void main(String[] args) 
+{
+    int[] elements  = {0 , 2 , 10 , 3, -3 }; 
+    int smallest = 0; 
+    int secondSmallest = 0; 
+
+    for (int i = 0; i < elements.length; i++)
+    {
+        for (int j = 0; j < elements.length; j++)
+        {
+            if (elements[i] < smallest)
+            {
+                smallest = elements[i];
+
+                if (elements[j] < secondSmallest)
+                {
+                    secondSmallest = elements[j];
+                }
+            }
+        }
+
+    }
+
+    System.out.println("The smallest element is: " + smallest + "\n"+  "The second smallest element is: " + secondSmallest);
+}
+}
